fcc55c4da051b293d0a841fa2ee8f415dcc1bcb0
Karim <karim.ali14199314@mail.com>
2024-10-21 17:34:33 +0300

    handle make request for location

--- lib/Presentation/Screens/GoogleMapScreen.dart
+++ lib/Presentation/Screens/GoogleMapScreen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class GoogleMapScreen extends StatefulWidget {
   const GoogleMapScreen({super.key});
@@ -21,6 +22,41 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
     controller.complete(_controller);
   }
 
+  String _permissionStatus = "Unknown";
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLocationPermission();
+  }
+
+  Future<void> _checkLocationPermission() async {
+    // Check the status of the location permission
+    PermissionStatus status = await Permission.location.status;
+
+    if (status.isGranted) {
+      setState(() {
+        _permissionStatus = "Location Permission Granted";
+      });
+    } else if (status.isDenied) {
+      // Request the permission
+      if (await Permission.location.request().isGranted) {
+        setState(() {
+          _permissionStatus = "Location Permission Granted";
+        });
+      } else {
+        setState(() {
+          _permissionStatus = "Location Permission Denied";
+        });
+      }
+    } else if (status.isPermanentlyDenied) {
+      setState(() {
+        _permissionStatus = "Location Permission Permanently Denied";
+      });
+      // Direct the user to app settings to enable permissions
+      openAppSettings();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
